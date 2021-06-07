75bb6a5132f3421e970f8d711f2e0c94e785b345
Ninja-Shark <82209169+Ninja-Shark@users.noreply.github.com>
2021-06-07 22:59:50 +0800

    add blackhole
    add blackhole

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 export default {
+    WHT: '0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F',
     token: '0x58EC55f2B564bf1f32A089236106Ab14F5f77f42',
     factory: '0x2DF36eF39AEE212f45Ba2B942d5991BF2a262eee',
     router: '0x14F67211fDe84b19de46f8f289bFc7442D9BC92B',
-    blackHole: '',
+    blackHole: '0x930FE10B08F05703B10d706fcC5095f30DF00Afd',
     invite: '',
     pool: '',
     repurchase: ''
